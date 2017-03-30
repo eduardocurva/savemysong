2c87510f3e5392195dbb034a3cefdc48384ad35a
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-03-30 16:33:10 +1300

    implementing controller and post data to firebase

--- app/components/recording/recording.js
+++ app/components/recording/recording.js
@@ -7,30 +7,52 @@
         return $http.get("/movies.json")
             .then(function (response) {
                 return response.data;
-            })
+            });
     }
 
-    function controller($http) {
+    function RecordingController($http, $scope, PlaylistFactory) {
 
         var self = this;
-        
+
 
         self.changeMessage = function () {
             self.message = "New message";
-        }
+        };
+        self.clear_scope = function () {
+            $scope.recording = '';
+        };
+        self.cancel_recording = function () {
+            self.clear_scope();
+        };
+
+        self.save_recording = function () {
+
+            PlaylistFactory.$add({
+                name: $scope.recording.name,
+                data: 'no_data'
+            }).then(function (response) {
+                //show confirmation data
+                console.log('Recording posted into the database', response.key);
+                self.clear_scope();
+
+                //save file into firebase storage
+            }).catch(function (error) {
+                console.log(error);
+            });
+        };
 
         self.$onInit = function () {
             /*     fetchData($http).then(function (dataList) {
                      self.dataList = dataList;
                  })
                  */
-        }
+        };
     }
 
     module.component('recording', {
         templateUrl: 'components/recording/recording.html',
         controllerAs: "recording",
-        controller: ["$http", controller]
-    })
+        controller: ["$http", "$scope", "PlaylistFactory", RecordingController]
+    });
 
 }());
